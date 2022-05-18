b5ec03bb1f79b2cb22e79f24c29b7d8567fd964a
UnsignedArduino <UnsignedArduino@outlook.com>
2022-05-17 21:20:03 -0400

    Can "cat" from the filesystem

--- src/filesystem.py
+++ src/filesystem.py
@@ -97,11 +97,33 @@ def _init_filesystem() -> dict[str, dict[str, dict[str, str], str, str]]:
 class Filesystem:
     _filesystem = _init_filesystem()
 
+    @staticmethod
+    def filesystem() -> dict[str, dict[str, dict[str, str], str, str]]:
+        return Filesystem._filesystem
+
 
 class Interface:
     @staticmethod
     def cat(path: str) -> str:
-        return ""
+        if path[0] != "/":
+            path = "/" + path
+        if path[-1] == "/":
+            path = path[:-1]
+        components = path.split("/")[1:]
+        fs = Filesystem.filesystem()
+        cwd = fs
+        for directory in components[:-1]:
+            if isinstance(cwd, str):
+                return f"cat: can't open '{path}': Not a directory"
+            elif directory not in cwd:
+                return f"cat: can't open '{path}': No such file or directory"
+            cwd = cwd[directory]
+        filename = components[-1]
+        if filename not in cwd:
+            return f"cat: can't open '{path}': No such file or directory"
+        elif isinstance(cwd[filename], dict):
+            return f"cat: read error: Is a directory"
+        return cwd[filename]
 
     @staticmethod
     def ls(path: str) -> str:
